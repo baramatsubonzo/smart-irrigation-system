e0659e40ebaa308f1c639bdc599529c31d77642f
Shota Matsubara <baramatsubonzo@gmail.com>
2025-10-16 21:47:26 +1000

    Stop motor in 5 sec automatically

--- tier1/arduino-smart-irrigation/arduino-smart-irrigation.ino
+++ tier1/arduino-smart-irrigation/arduino-smart-irrigation.ino
@@ -1,7 +1,7 @@
 #include <ArduinoBLE.h>
 
 const int SOIL_PIN = A0;
-const int PUMP_PIN = LED_BUILTIN;  // 物理ポンプが無い間はLEDで代用
+const int PUMP_PIN = 8;
 
 BLEService soilService("19B10010-E8F2-537E-4F6C-D104768A1214");
 
@@ -16,6 +16,9 @@ BLEByteCharacteristic pumpChar(
   "19B10012-E8F2-537E-4F6C-D104768A1214",
   BLEWrite | BLEWriteWithoutResponse
 );
+
+unsigned long pumpStartTime = 0;
+bool pumpIsOn = false;
 
 void setup() {
   Serial.begin(115200);
@@ -56,8 +59,24 @@ void loop() {
       if (pumpChar.written()) {
         byte cmd = 0;
         pumpChar.readValue(cmd);
-        if (cmd == 1) { digitalWrite(PUMP_PIN, HIGH); Serial.println("PUMP: ON"); }
-        else          { digitalWrite(PUMP_PIN, LOW);  Serial.println("PUMP: OFF"); }
+        if (cmd == 1 && !pumpIsOn) {
+          digitalWrite(PUMP_PIN, HIGH);
+          pumpIsOn = true;
+          pumpStartTime = millis();
+          Serial.println("PUMP: ON (for 5 seconds)");
+        }
+        else if (cmd == 0 && pumpIsOn){
+          digitalWrite(PUMP_PIN, LOW);
+          pumpIsOn = false;
+          Serial.println("PUMP: OFF (manual operation)");
+        }
+      }
+
+      if (pumpIsOn && (millis() - pumpStartTime >= 5000)) {
+        digitalWrite(PUMP_PIN, LOW);
+        pumpIsOn = false;
+        pumpChar.writeValue((byte)0);
+        Serial.println("PUMP: OFF (auto timer)");
       }
 
       // 1秒ごとに土壌値Notify
@@ -68,5 +87,11 @@ void loop() {
       }
     }
     Serial.println("Disconnected");
+
+    if(pumpIsOn) {
+      digitalWrite(PUMP_PIN, LOW);
+      pumpIsOn = false;
+      Serial.println("PUMP: OFF (disconnected)");
+    } 
   }
 }
